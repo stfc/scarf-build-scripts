3d58bd10bf2dea85f4764bc311ef29b7f7883caa
Derek Ross <derek.ross@stfc.ac.uk>
2016-11-04 16:00:41 +0000

    Files for building hpl 2.2

---
+++ hpl/2.2/build.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+
+APPNAME="hpl"
+VERSION="2.2"
+ARCH="Linux_Intel64"
+
+module load intel/16.1
+module load intel/mkl/11.3.1.150
+
+BUILDDIR=`mktemp -d`
+cp Make.${ARCH} ${BUILDDIR}
+cd ${BUILDDIR}
+
+tar xvzf /apps/src/${APPNAME}/${APPNAME}-${VERSION}.tar.gz
+cp Make.${ARCH} ${APPNAME}-${VERSION}/
+       
+mkdir -p /apps/procspec/${APPNAME}/${VERSION}/bin/${ARCH}
+
+cd ${APPNAME}-${VERSION}
+sed -i -e "s#^TOPdir.*#TOPdir = ${BUILDDIR}/${APPNAME}-${VERSION}#"  Make.${ARCH}
+
+make arch=${ARCH} install
+
+cd ../..
+rm -fr ${BUILDDIR}
+
+
+
+
+
+
+
+
+
